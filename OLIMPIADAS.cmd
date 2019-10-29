a0d30756aa2ae12c9f5c69b47de14afcee2a57c2
davidbestue <davidsanchezbestue@hotmail.com>
2019-10-29 11:49:39 +0100

    Update OLIMPIADAS.cmd

--- OLIMPIADAS.cmd
+++ OLIMPIADAS.cmd
@@ -1,1 +1,1 @@
-"C:\Users\David\Anaconda3\python.exe""C:\Users\David\Documents\Github\code_olimpiadas\resultados_olimpiadas.py"
+"C:\Users\David\Anaconda3\python.exe" "C:\Users\David\Documents\GitHub\code_olimpiadas\resultados_olimpiadas.py"
